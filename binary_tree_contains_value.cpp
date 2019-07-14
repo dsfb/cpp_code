6f62975c8624afe4ca61735edf39bf0ea1abe25c
Aleksander Czastuhin <bestdevappcpp@gmail.com>
2019-07-14 16:43:58 +0200

    second version

--- binary_tree_contains_value.cpp
+++ binary_tree_contains_value.cpp
@@ -1,78 +1,125 @@
-#include <stdexcept>
+/*
+you need create a binary tree 
+you must rwite a method to check if a value contains or not
+Aleksander Czastuchin
+first version - 2017
+second verison - 2019
+
+g++ -std=c++17 binary_tree_contains_value.cpp -o main.exe  && ./main.exe
+ */
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-class Node
+struct node
 {
-public:
-	Node(int value, Node* left, Node* right)
+	node()
 	{
-		this->value = value;
-		this->left = left;
-		this->right = right;
+		left = nullptr;
+		right = nullptr;
+		value = 0;
 	}
-	int getValue() const
-	{
-		return value;
-	}
-	Node* getLeft() const
-	{
-		return left;
-	}
-	Node* getRight() const
-	{
-		return right;
-	}
-
-private:
 	int value;
-	Node* left;
-	Node* right;
+	node* left;
+	node* right;
 };
 
-class BinarySearchTree
+struct binary_tree : node
 {
-public:
-	static bool contains(const Node& root, int value)
+	static bool contains(const node* leaf, const int& value)
 	{
-		/*
-		    20
-		  11   21
-		10 5      25
-		*/
-		Node* elem = nullptr;
-
-		if (root.getValue() == value)
-		{
+		if (leaf->value == value)
 			return true;
-		}
-		else if (root.getValue() < value)
-			elem = root.getLeft();
-		else elem = root.getRight();
-
-		while (elem)
-		{
-			if (left && elem->getValue() == value)
+		
+		if (leaf->left)
+			if (contains(leaf->left, value))
 				return true;
-			else if (elem->getValue() < value)
-				elem = elem->getLeft();
-			else if (elem->getValue() > value)
-				elem = elem->getRight();
-		}
+		
+		if (leaf->right)
+			if (contains(leaf->right, value))
+				return true;
 
 		return false;
 	}
+
+	template <typename type, typename ... types>
+	void add_pack(type arg, types ... args)
+	{
+		add_leaf(arg);
+		add_pack(args ...);
+	}
+
+	template <typename type>
+	void add_pack(type arg)
+	{
+		add_leaf(arg);
+	}
+
+	void add_leaf(const int& value)
+	{
+		add_leaf_static(this, value);
+	}
+
+	static void add_leaf_static(node* leaf, const int& value)
+	{
+		if (leaf->value == 0){
+			leaf->value = value;
+			return;
+		}
+
+		bool is_left = value <= leaf->value;
+		bool is_right = value > leaf->value;
+
+		if (is_left && leaf->left) {
+			add_leaf_static(leaf->left, value);
+			return;
+		}
+		else if (is_left) {
+			leaf->left = new node();
+			leaf->left->value = value;
+			return;
+		}
+
+		if (is_right && leaf->right) {
+			add_leaf_static(leaf->right, value);
+			return;
+		}
+		else if (is_right) {
+			leaf->right = new node();
+			leaf->right->value = value;
+			return;
+		}
+	}
+
+	static void print(node* leaf)
+	{
+		cout << leaf->value;
+		cout << ' ';
+
+		if (leaf->left)
+			print(leaf->left);
+
+		if (leaf->left == nullptr && leaf->right == nullptr)
+		cout << endl;
+		
+		if (leaf->right)
+			print(leaf->right);	
+	}
 };
 
-#ifndef RunTests
 int main()
 {
-	Node n1(1, NULL, NULL);
-	Node n3(3, NULL, NULL);
-	Node n2(2, &n1, &n3);
+	binary_tree tree;
+	tree.add_pack(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+	tree.add_pack(20, 30, 40, 50, 60, 70);
+	tree.add_pack(21, 22, 23, 24, 25, 25);
+	binary_tree::print(&tree);
 
-	std::cout << BinarySearchTree::contains(n3, 3);
+	cout << boolalpha;
+	cout << binary_tree::contains(&tree, 22) << endl;
+	cout << binary_tree::contains(&tree, 100) << endl;
+
+
+	return 0;
 }
-#endif
