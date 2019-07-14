4ff0979f0eb9a067679446598cf4b1bd0b32bad2
Aleksander Czastuhin <bestdevappcpp@gmail.com>
2019-07-14 17:09:36 +0200

    added inheritance_how_it_works.cpp project

---
+++ inheritance_how_it_works.cpp
@@ -0,0 +1,51 @@
+/*show me how works an inharitance in C++ */
+
+// kształt
+class shape
+{
+public:
+	// virtual is mandatory for a polimorphic class
+	virtual int get_area() = 0;
+	// virtual means: I want to use last available method - said the pointer.
+	// it calls "late binding"
+	// moreover, the destructor must have a body in order to delete the object
+	// (child will call him after itself)
+	virtual ~shape() {};
+};
+
+class rectangle : public shape
+{
+public:
+	// virtual is optional for child' class
+	virtual int get_area() override {
+		return height * width;
+	} 
+protected:
+	float width; // szerokość
+	float height; // wysokość
+};
+
+// trójkąt
+class triangle : public shape
+{
+public: 
+	int get_area() override {
+		return (1.f/2.f) * side * height;
+	}
+private:
+	float side; // strona
+	float height; // wysokość
+};
+
+int main()
+{
+	rectangle* r = new rectangle();
+	triangle* t = new triangle();
+	shape* s = nullptr;
+	s = t;
+	s->get_area();
+	delete s;
+	s = t;
+	delete s;
+	return 0;
+}
