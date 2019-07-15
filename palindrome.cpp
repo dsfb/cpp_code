3de3156138074bfc192dcb0003633d1254fac060
Aleksander Czastuhin <bestdevappcpp@gmail.com>
2019-07-15 13:04:52 +0200

    palindrome added

---
+++ palindrome.cpp
@@ -0,0 +1,48 @@
+/*
+you need write a polindrom function.
+Palindrom – wyrażenie brzmiące tak samo czytane od lewej do prawej i od prawej do lewej.
+ Przykładem palindromu jest: Kobyła ma mały bok */
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(const std::string& word)
+{
+		auto start = word.begin();
+		auto end = --word.end();
+		int s = word.size();
+		unsigned short a, b;
+
+		for (int i = 0; i <= s; i++)
+		{
+			a = *start;
+			b = *end;
+			//  D(68) = d(100)
+			// a == a     a == A         A == a
+			if (a == b || a == b - 32 || b == a - 32)
+			{
+				continue;
+			}
+			else
+			{
+				return false;
+			}
+			start++;
+			end--;
+		}
+		return true;
+}
+
+
+int main()
+{
+	char a = 'D';
+	//char b = 'd';
+	//auto r = (int)a - (int)b;
+	//cout << r << endl;
+	cout << Palindrome::isPalindrome("Deleveled");
+	cin >> a;
+	return 0;
+}
