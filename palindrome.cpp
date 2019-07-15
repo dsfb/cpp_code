2626f612fdf77378df62c4b97a63f4d697d1df43
Aleksander Czastuhin <bestdevappcpp@gmail.com>
2019-07-15 15:14:50 +0200

    refactoring

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,7 +1,11 @@
-/*
-you need write a polindrom function.
+/* you need write a polindrom function.
 Palindrom – wyrażenie brzmiące tak samo czytane od lewej do prawej i od prawej do lewej.
- Przykładem palindromu jest: Kobyła ma mały bok */
+ Przykładem palindromu jest: Kobyła ma mały bok
+
+ Aleksander Czastuchin
+ 2017
+ g++ -std=c++17 palindrome.cpp -o main.exe
+  */
 #include <string>
 #include <stdexcept>
 #include <iostream>
@@ -10,39 +14,39 @@ using namespace std;
 
 bool isPalindrome(const std::string& word)
 {
-		auto start = word.begin();
-		auto end = --word.end();
-		int s = word.size();
-		unsigned short a, b;
+    auto start = word.begin();
+    auto end = --word.end();
+    int s = word.size();
+    unsigned short a, b;
 
-		for (int i = 0; i <= s; i++)
-		{
-			a = *start;
-			b = *end;
-			//  D(68) = d(100)
-			// a == a     a == A         A == a
-			if (a == b || a == b - 32 || b == a - 32)
-			{
-				continue;
-			}
-			else
-			{
-				return false;
-			}
-			start++;
-			end--;
-		}
-		return true;
+    for (int i = 0; i <= s; i++)
+    {
+        a = *start;
+        b = *end;
+        //  D(68) = d(100)
+        // a == a     a == A         A == a
+        if (a == b || a == b - 32 || b == a - 32)
+        {
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
 }
 
 
 int main()
 {
 	char a = 'D';
-	//char b = 'd';
-	//auto r = (int)a - (int)b;
-	//cout << r << endl;
-	cout << Palindrome::isPalindrome("Deleveled");
-	cin >> a;
+	char b = 'd';
+	auto r = (int)a - (int)b;
+	cout << r << endl;
+    cout << boolalpha;
+	cout << isPalindrome("Deleveled");
 	return 0;
 }
