7396104fd988de43a9093792afa5fa81d71d75b0
Aleksander Czastuhin <oleksandr@sims.pl>
2019-08-05 11:56:40 +0200

    bubble sort added

---
+++ bubble_sort.cpp
@@ -0,0 +1,46 @@
+#include <array>
+#include <iostream>
+
+using std::array;
+using std::cout;
+using std::endl;
+
+void print_array(array<short, 10>& numbers)
+{
+    for (const auto n : numbers)
+        cout << n << ',';
+
+    //cout << '\n';
+}
+
+void bubble_sort(array<short, 10>& numbers)
+{
+    short iteration_count = 0;
+    for (int k = numbers.size() - 1; k >= 0; --k) {
+        for (int i = 0; i < k; i++) {
+            //cout << "iteration nr " << ++iteration_count << endl;
+            cout << numbers[i] << " and " << numbers[i + 1];
+            if (numbers[i] > numbers[i + 1]) {
+                cout << " -> ";
+                short temp = numbers[i];
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = temp;
+                print_array(numbers);
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    using type = short;
+    array<type, 10> numbers = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+    print_array(numbers);
+    cout << '\n';
+    bubble_sort(numbers);
+    cout << '\n';
+    print_array(numbers);
+    return 0;
+}
