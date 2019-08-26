a7fc0b77b6bcf7c75897371d2094a02b0f8360ef
Aleksander Czastuhin <oleksandr@sims.pl>
2019-08-26 15:10:25 +0200

    update

--- bash/print_file_names_by_type.bash
+++ bash/print_file_names_by_type.bash
@@ -4,21 +4,21 @@
 rm -f *.log
 
 echo "images/"
-echo `find -type f -iname "*.png" | sed "s/^..//g"`
-echo `find -type f -iname "*.jpg" | sed "s/^..//g"`
+echo `find . -type f -iname "*.png" | sed "s/^..//g"`
+echo `find . -type f -iname "*.jpg" | sed "s/^..//g"`
 echo "music/"
-echo `find -type f -iname "*.mp3" | sed "s/^..//g"`
-echo `find -type f -iname "*.flac" | sed "s/^..//g"`
+echo `find . -type f -iname "*.mp3" | sed "s/^..//g"`
+echo `find . -type f -iname "*.flac" | sed "s/^..//g"`
 echo "videos/"
-echo `find -type f -iname "*.mov" | sed "s/^..//g"`
-echo `find -type f -iname "*.avi" | sed "s/^..//g"`
-#echo `find -type f -iname "*.txt" | sed "s/^..//g"`
-#echo `find -type f -iname "*.jpeg" | sed "s/^..//g"`
-#echo `find -type f -iname "[0-9]*" | sed "s/^..//g"`
+echo `find . -type f -iname "*.mov" | sed "s/^..//g"`
+echo `find . -type f -iname "*.avi" | sed "s/^..//g"`
+#echo `find . -type f -iname "*.txt" | sed "s/^..//g"`
+#echo `find . -type f -iname "*.jpeg" | sed "s/^..//g"`
+#echo `find . -type f -iname "[0-9]*" | sed "s/^..//g"`
 
 # I print another files
 extensions="*.png|*.jpg|*.mp3|*.flac|*.mov|*.avi"
-echo `find -type f -not -name "Sextensions"  | sed "s/^..//g"`
+echo `find . -type f -not -name "Sextensions"  | sed "s/^..//g"`
 
 
 # Aleksander Czastuchin
