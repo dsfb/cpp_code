d7cd8e04cdc30468a7367238a35dd6c3ee22f89b
sasza <a.czastuchin@sims.pl>
2019-09-02 15:48:42 +0200

    update

---
+++ bash/must_to_have.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+alias inst='sudo apt-get install'
+
+inst gcc-8 gcc-8-multilib 
+inst g++-8 g++-8-multilib
+
+inst pkg-config
+inst vim
+inst apt-file
+
+inst libgstreamer1.0-dev
+inst libgstreamer1.0-0
+inst libltdl7
+inst libltd7:i1386
+inst libltdl7:i386
+inst libmpg123-0:i386
+inst libout123-0:i386
+inst  libsystemd-dev
+
