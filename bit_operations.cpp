31e863966e4ef4800570cbc08c9bb1c55eac9d09
Aleksander Czastuchin <a.czastuchin@sims.pl>
2019-10-07 12:17:57 +0200

    decimal to binary convert function added

--- bit_operations.cpp
+++ bit_operations.cpp
@@ -15,8 +15,22 @@ Aleksander Czastuchin
 
 using namespace std;
 
+#define nl '\n'
 #define bits(x) std::bitset<sizeof(x) * 8>(x)
 #define uint32 unsigned
+
+// it change decimal to binary
+string to_binary(short decimal)
+{
+    string result;
+    while(decimal > 0) {
+        short if_modulo = (decimal % 2) > 0 ? 1 : 0;
+        result = to_string(if_modulo) + result; // [new bit][old bit]
+        decimal = decimal / 2;
+    }
+
+    return result;
+}
 
 uint32 set_ip_address(uint32 one, uint32 two, uint32 three, uint32 four)
 {
@@ -45,12 +59,30 @@ void print_ip_address(const uint32& ip)
         //cout << bits((ip >> 8 * i)) << endl;   
         cout << ((ip >> 8 * i) & mask) << '.';
     }
+
+    cout << nl;
+}
+
+void print_ip_address_binary(const uint32& ip)
+{
+    cout << std::dec;
+    uint32 mask = 0;
+    mask = mask | 255;
+    //cout << bits(mask) << endl;
+    for (short i = 3; i >= 0; i--)
+    {
+        //cout << bits((ip >> 8 * i)) << endl;   
+        cout << to_binary((ip >> 8 * i) & mask).c_str() << '.';
+    }
+
+    cout << nl;
 }
 
 int main()
 {
-    auto&& ip = set_ip_address(255, 200, 150, 50);
+    auto&& ip = set_ip_address(180, 200, 150, 50);
     print_ip_address(ip);
+    print_ip_address_binary(ip);
     return 0;
 
 }
