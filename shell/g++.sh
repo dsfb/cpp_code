0be56fcf049e508e4974152b07984e22ba472f37
Oleksandr Chastukhin <oleksandr.chastukhin@nokia.com>
2020-01-29 14:22:57 +0100

    g++ update

--- shell/g++.sh
+++ shell/g++.sh
@@ -47,3 +47,33 @@ For details:
 
 (gdb) help exec-file
 (gdb) help symbol-file
+# how to install g++ compiler
+sudo apt-get update
+
+# to find some g++ or gcc compiler version
+sudo apt-cache search g++ | grep "g++-8"
+
+# pokazać info dotyczące owego pakietu
+apt-cache show g++-8
+
+# installation
+sudo apt-get install g++-8 # cpp-compiler
+sudo apt-get install gcc-8 # c-compiler
+
+# linking (tworzę skrót o nazwie g++, z którego korzystają środowiska)
+cd /usr/bin
+sudo rm g++ # if exist
+sudo ln -s g++-8 g++
+
+# how to test
+whereis g++
+file g++
+g++ --version
+
+# where installed g++ files
+sudo dpkg -L PACKAGE_NAME
+
+
+***
+
+
