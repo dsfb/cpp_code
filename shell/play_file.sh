6db12763825200b5bfd0fa90c8ec18adedd0ed49
sasza <sawer_5@mail.ru>
2020-02-01 15:35:36 +0100

    player was improved

--- shell/play_file.sh
+++ shell/play_file.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # install 
-# 2) give a permissions
+# 1) give a permission
 # chmod u+x play_file.sh
 # alias inst='sudo apt-get install'
-# cd /mnt/c && mkdir code && cd code
+# cd /mnt/c && mkdir code && mkdir en_audio && cd code
 # git clone
 # alias enp='/mnt/c/code/cpp_code/shell/./play_file.sh'
 # run cmd and compile an app
@@ -22,7 +22,7 @@ fi
 
 line_count=`cat $1 | grep -v ^$ | wc --lines`
 current_line=1
-audio_file=""
+#audio_file=""
 
 echo "playing a file: $1"
 echo "the file has $line_count lines"
@@ -41,82 +41,72 @@ function replace()
 
 while [ $current_line -le $line_count ]
 #while [ $current_line -le 2 ]
-do 
+do
+    # get a next line
     cd $start_folder
     line=`cat $1 | sed -n ${current_line}p`
+
+    # remove useless symbols
+    original_line=$line
     line=`echo $line | tr ',' ' '`
-    #line=`echo $line | tr 'smth' 'something'`
-    #replace "$line"
-    file_name_part_one=`echo $line | cut -d- -f1`
-    file_name__withount_pipes=`echo $file_name_part_one | sed 's/|.*|//g'`
-    file_name__withount_braces=`echo $file_name__withount_pipes | sed 's/(.*)//g'`
-    audio_file[0]=`echo $file_name__withount_braces | cut -d' ' -f1`
-    audio_file[1]=`echo $file_name__withount_braces | cut -d' ' -f2`
-    audio_file[2]=`echo $file_name__withount_braces | cut -d' ' -f3`
-    audio_file[3]=`echo $file_name__withount_braces | cut -d' ' -f4`
-    audio_file[4]=`echo $file_name__withount_braces | cut -d' ' -f5`
-    #echo ${audio_file[*]}
+    line=`echo $line | sed 's/|.*|//g'` # pipes
+    line=`echo $line | sed 's/(.*)//g'` # braces
+    line=`echo $line | sed 's/smth/something/g'` # something
+    line=`echo $line | sed 's/smb/somebody/g'` # somebody
+    line=`echo $line | sed 's/sb/somebody/g'` # somebody
+    line=`echo $line | cut -d- -f1` # first part
+
+    # compute a word count
+    word_count=`echo $line | wc --words`
+
+    # get file names
+    audio_file=""
+    index=0
+    word_number=1
+    while [ $word_number -le $word_count ]
+    do
+        audio_file[index]=`echo $line | cut -d' ' -f${word_number}`
+        let word_number++
+        let index++
+    done
+
+    # testing
+    #echo "file names = ${audio_file[*]}"
+    #echo "line = $line"
+    #echo "word count = $word_count"; echo ""
+    #let current_line++
     #sleep 1
+    #unset audio_file
     #continue
 
-    #if [[ $string == *"My long"* ]]; then
-        #echo "It's there!"
-    #fi
-
-    # if there is no file
-    if [ ! -f "$audio_folder/${audio_file[0]}.mp3" ]
-    then 
-        cd $audio_folder
-        wget --quiet https://wooordhunt.ru/data/sound/word/us/mp3/${audio_file[0]}.mp3
-    fi
-    if [ ! -f "~/en_audio/${audio_file[1]}.mp3" ]
-    then 
-        cd $audio_folder
-        wget --quiet https://wooordhunt.ru/data/sound/word/us/mp3/${audio_file[1]}.mp3
-    fi
-    #if [ ! -f "~/en_audio/${audio_file[2]}.mp3" ]
-    #then 
-        #echo "https://wooordhunt.ru/data/sound/word/us/mp3/${audio_file}.mp3"
-        #cd $audio_folder
-        #echo "downloading an audio"
-        #wget --quiet https://wooordhunt.ru/data/sound/word/us/mp3/${audio_file[2]}.mp3
-        #cd $old_pwd
-    #fi
-    #if [ ! -f "~/en_audio/${audio_file[3]}.mp3" ]
-    #then 
-        #echo "https://wooordhunt.ru/data/sound/word/us/mp3/${audio_file}.mp3"
-        #cd $audio_folder
-        #wget --quiet https://wooordhunt.ru/data/sound/word/us/mp3/${audio_file[3]}.mp3
-        #cd $old_pwd
-    #fi
-    #echo "file = $audio_file"
-    #file  "~/en_audio/${audio_file}.mp3"
-    
+    # pick a current folder
     cd $audio_folder
-    #clear
-    echo $line
-    #echo ""; echo "$current_line / $line_count"
-    ./play.exe "${audio_file[0]}.mp3"
 
-    if [ "${audio_file[0]}" != "${audio_file[1]}" ]
-    then
-        ./play.exe "${audio_file[1]}.mp3" 
-    fi
-    if [ "${audio_file[1]}" != "${audio_file[2]}"  ]
-    then
-        ./play.exe "${audio_file[2]}.mp3"
-    fi
-    #if [ "${audio_file[2]}" != "${audio_file[3]}"  ]
-    #then
-        #play ~/en_audio/${audio_file[3]}.mp3 
-    #fi
+    # a loop for downloding audio files
+    for file in "${audio_file[@]}"
+    do
+        # if there is no such file
+        if [ ! -f "$file.mp3" ]
+        then
+            wget --quiet https://wooordhunt.ru/data/sound/word/us/mp3/$file.mp3
+        fi
+    done
+    
+    clear
 
-    #echo `cat $1 | sed -n ${current_line}p | cut -d- -f1 ` 
-    #xterm -e "bash -c \"play ~/en_audio/${audio_file}.mp3 && exit; exec bash\"" &
+    # show a line and print a line number
+    echo $original_line
+    echo ""; echo "$current_line / $line_count"
+
+    # a loop for playing
+    for file in "${audio_file[@]}"
+    do
+        ./play.exe "$file.mp3"
+    done
+
+    unset audio_file
     let current_line++
-    #clear
     sleep 2
-    #exit 0
 done
 
 exit 0
