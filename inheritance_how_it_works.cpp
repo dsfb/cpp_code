2d62e27f71a1c480fb2970814feafcefa89adebf
Aleksander <52190011+cppcode1@users.noreply.github.com>
2020-09-15 11:50:45 +0200

    Update inheritance_how_it_works.cpp

--- inheritance_how_it_works.cpp
+++ inheritance_how_it_works.cpp
@@ -7,15 +7,17 @@ public:
 	// virtual keyword is mandatory for a polimorphic class
 	// =0 means that a function must be defined in a child class
 	virtual int get_area() = 0;
-	// virtual means: I want to use last available method - said a pointer.
-	// it is called "late binding"
-	// moreover, a destructor must have a body in order to delete the object
+	// 1) if a destructor of a base class is virtial, a child destructor becomes virtual too
+	// 2) virtual means: I want to use last available method - said a pointer.
+	// 3) it is called "late binding"
+	// 4)moreover, a destructor must have a body in order to delete the object
 	// (child will call him after its destructor)
 	virtual ~shape() {};
 };
 
 class rectangle : public shape
 {
+	// there is a hidden virtual destructor
 public:
 	// "virtual" keyword is optional for a child class
 	virtual int get_area() override {
@@ -29,6 +31,7 @@ protected:
 // trójkąt
 class triangle : public shape
 {
+	// there is a hidden virtual destructor
 public: 
 	int get_area() override {
 		return (1.f/2.f) * side * height;
